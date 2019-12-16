ff3398be4ddd05afe3bad0564e86a14dbe0618c2
Callum Fanshawe <callum.fanshawe@autotrader.co.uk>
2019-12-16 16:15:56 +0000

    changed create route to /notes

--- api/app/routes/note.routes.js
+++ api/app/routes/note.routes.js
@@ -2,7 +2,7 @@ module.exports = (app) => {
     const notes = require('../controllers/note.controller.js');
 
     // Create a new Note
-    app.post('/', notes.create);
+    app.post('/notes', notes.create);
 
     // Retrieve all Notes
     app.get('/notes', notes.findAll);
