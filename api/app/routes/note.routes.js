332f55a8ed32d340c2a9211a7537136b34cacf28
Callum Fanshawe <callum.fanshawe@autotrader.co.uk>
2020-01-07 17:05:05 +0000

    added new note and delete note feature. Fixed deleting on refresh

--- api/app/routes/note.routes.js
+++ api/app/routes/note.routes.js
@@ -2,17 +2,17 @@ module.exports = (app) => {
     const notes = require('../controllers/note.controller.js');
 
     // Create a new Note
-    app.post('/notes', notes.create);
+    app.post('/api/notes', notes.create);
 
     // Retrieve all Notes
-    app.get('/notes', notes.findAll);
+    app.get('/api/notes', notes.findAll);
 
     // Retrieve a single Note with noteId
-    app.get('/notes/:noteId', notes.findOne);
+    app.get('/api/notes/:noteId', notes.findOne);
 
     // Update a Note with noteId
-    app.put('/notes/:noteId', notes.update);
+    app.put('/api/notes/:noteId', notes.update);
 
     // Delete a Note with noteId
-    app.delete('/notes/:noteId', notes.delete);
+    app.delete('/api/notes/:noteId', notes.delete);
 }
