e91fba2d1b2d6595775f9cd327c5b949075b07cf
Callum Fanshawe <callum.fanshawe@autotrader.co.uk>
2020-01-29 16:09:01 +0000

    added onclick function to render singleNoteView component

--- src/components/main.panel.js
+++ src/components/main.panel.js
@@ -2,10 +2,17 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import '../App.css';
 import {useGlobalState} from './state'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+import { SingleNoteView } from './single.note';
 
 export default function MainFunc() {
   const className = 'notes'
   const [data, setData] = useGlobalState('data');
+  const [currentNote, setCurrentNote] = useState()
 
   const fetchData = async () => {
     const result = await axios(
@@ -21,17 +28,26 @@ export default function MainFunc() {
     axios.delete(`http://localhost:3001/api/notes/${noteId}`).then( () => fetchData())
   }
 
+  const selectSingleNote = (noteId) => { 
+     setCurrentNote(noteId)
+  }
+
   return (
     <>
       <ul className={`${className}__list`}>
       {data.map(item => (
-        <li key={item._id} className="notes__item">
+        <li key={item._id} className="notes__item" onClick={() => {selectSingleNote(item._id)}}>
           {<p className={`${className}__item-title`}>{item.title}</p>}
           {<p>{item.content}</p>}
           <div onClick={() => handleClick(item._id)}>x</div>
         </li>
       ))}
       </ul>
+      <div>
+        {currentNote && (
+          <SingleNoteView cn={currentNote} />
+        )}
+      </div>
     </>
   );
 }
