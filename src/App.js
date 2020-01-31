6c810288f570acfdd865c8673bf1620d93ee19ac
Callum Fanshawe <callum.fanshawe@autotrader.co.uk>
2020-01-31 16:29:20 +0000

    Added toolbar to single note and added inputs for single note

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import axios from 'axios';
 import './App.css';
 import MainFunc from './components/main.panel'
 import { SideBar } from './components/sidebar'
-import { SingleNoteView } from './components/single.note'
 
 function App() {
   return (
--- src/components/single.note.js
+++ src/components/single.note.js
@@ -1,12 +1,17 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios';
 import { useGlobalState } from './state';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+import { faShareAlt } from '@fortawesome/free-solid-svg-icons'
+import { faBookmark } from '@fortawesome/free-solid-svg-icons'
 
 export const SingleNoteView = ({cn}) => {
 
     const [singleData, setSingleData] = useState();
     const [data, setData] = useGlobalState('data');
 
+    const className = 'single-note'
 
     const fetchSingleData = async () => {
       const result = await axios(
@@ -29,15 +34,46 @@ export const SingleNoteView = ({cn}) => {
       const deleteNote = () => {
         axios.delete(`http://localhost:3001/api/notes/${cn}`).then(() => fetchData() & setSingleData(null))
       }
+
+      const formattedDate = (nPDate) => {
+        const createdDateParsed = new Date(Date.parse(nPDate))
+        const day = createdDateParsed.getDate()
+        const monthData = createdDateParsed.getMonth()
+        const year = createdDateParsed.getFullYear()
+
+        const month = ["January","February","March","April","May","June","July","August","September","October","November","December",];
+
+        const formattedString = day + ' ' + month[monthData] + ' ' + year
+
+        return formattedString
+
+      }
+
+      const ToolBar = () => {
+        return (
+          <div className={`${className}__toolbar`}>
+            <span className={`${className}__toolbar-date`}>{formattedDate(singleData.createdAt)}</span>
+            <div className={`${className}__toolbar-icons-wrapper`}>
+              <ul className={`${className}__icons`}>
+                <li><FontAwesomeIcon icon={faShareAlt} /></li>
+                <li><FontAwesomeIcon icon={faBookmark} /></li>
+                <li><FontAwesomeIcon icon={faTrashAlt} className='icon-trash' onClick={() => deleteNote()} /></li>
+              </ul>
+            </div>
+          </div>
+        )
+      }
       
       return (
         <div>
           {singleData &&
-            <>
-              <p className={`__item-title`}>{singleData.title}</p>
-              <p>{singleData.content}</p>
-              <div onClick={() => deleteNote()}>x</div>
-            </>
+            <div className={`${className}__wrapper`}>
+            <ToolBar />
+              <div className={`${className}__inner`}>
+                <input type="text" className={`${className}__title`} defaultValue={singleData.title} onChange=''/>
+                <textarea defaultValue={singleData.content} />
+              </div>
+            </div>
           }     
         </div>
       )
