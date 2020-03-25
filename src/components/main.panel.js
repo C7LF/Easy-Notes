16c49355d1067b477499a95cb11107d759e8dd5c
Cal <callummailbox@gmail.com>
2020-03-25 23:04:43 +0000

    Change List:
    	- Added local storage for noteId, on reload continues on last
    	note
    	- Added active class with styles
    	- Altered CSS.
    	- Added preview text on list

--- src/components/main.panel.js
+++ src/components/main.panel.js
@@ -4,12 +4,14 @@ import '../App.css';
 import {useGlobalState} from './state'
 import { SingleNoteView } from './single.note';
 import {Editor, convertToHTML, EditorState, ContentState, convertFromRaw, convertToRaw} from 'draft-js';
+import { faSubscript } from '@fortawesome/free-solid-svg-icons';
+import ls from 'local-storage'
 
 
 export default function MainFunc() {
   const className = 'notes'
   const [data, setData] = useGlobalState('data');
-  const [currentNote, setCurrentNote] = useState()
+  const [currentNote, setCurrentNote] = useState(localStorage.getItem('Note Id'))
 
   const fetchData = async () => {
     const result = await axios(
@@ -21,17 +23,28 @@ export default function MainFunc() {
     fetchData();
   }, []);
 
-  const selectSingleNote = (noteId) => { 
-     setCurrentNote(noteId)
+  const selectSingleNote = (noteId) => {
+    localStorage.setItem('Note Id', noteId)
+    setCurrentNote(noteId)
   }
+
+  const objectPreviewText = (x) => {
+    const allText = x.blocks.map(block => (!block.text.trim() && ' ') || block.text).join(' ')
+    const previewStr = allText.length > 10 ? allText.substring(0, 10) + "..." : allText
+
+    return previewStr
+  }
+
+  const checkForActive = (currentId) => (localStorage.getItem('Note Id') == currentId) && 'active'
+
 
   return (
     <>
       <ul className={`${className}__list`}>
       {data.map(item => (
-        <li key={item._id} className="notes__item" onClick={() => {selectSingleNote(item._id)}}>
-          {<p className={`${className}__item-title`}>{item.title}</p>}
-          {<p>{JSON.parse(item.content)}</p>}
+        <li key={item._id} className={`notes__item ${checkForActive(item._id)}`} onClick={() => {selectSingleNote(item._id)}}>
+          <p className={`${className}__item-title`}>{item.title}</p>
+          <p>{objectPreviewText(JSON.parse(item.content))}</p>
         </li>
       ))}
       </ul>
