c27368202a29f4fa22fb9cad42ba68c6330f5bbf
Cal <callummailbox@gmail.com>
2020-04-17 00:29:31 +0100

    Fixed draft-js markdown hooks error
    implemented temporary fix found here:
    https://www.npmjs.com/package/draft-js-md-keyboard-plugin
    
    ** needs to be updated to offical fix when merged

--- src/components/single.note.js
+++ src/components/single.note.js
@@ -6,8 +6,9 @@ import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 import { faShareAlt } from '@fortawesome/free-solid-svg-icons'
 import { faBookmark } from '@fortawesome/free-solid-svg-icons'
 import Editor from 'draft-js-plugins-editor';
-import createMarkdownPlugin from 'draft-js-markdown-plugin';
-import { EditorState, ContentState, convertFromRaw, convertToRaw} from 'draft-js';
+//import createMarkdownPlugin from 'draft-js-markdown-plugin';
+import createMarkdownShortcutsPlugin from 'draft-js-md-keyboard-plugin';
+import { EditorState, convertFromRaw, convertToRaw} from 'draft-js';
 import 'draft-js/dist/Draft.css'
 import { Modal } from './modal'
 
@@ -107,12 +108,13 @@ export const SingleNoteView = ({cn}) => {
     const changeText = editorState => {
       setEditorState(editorState)
 
-      // const newData = { 
-      //   title: singleData.title, 
-      //   content: JSON.stringify(convertToRaw(editorState.getCurrentContent()))
-      // }
+      const newData = { 
+        title: singleData.title, 
+        content: JSON.stringify(convertToRaw(editorState.getCurrentContent()))
 
-      // axios.put(`http://localhost:3001/api/notes/${cn}`, newData)
+      }
+
+      axios.put(`http://localhost:3001/api/notes/${cn}`, newData)
     }
 
     const handleChange = (e) => {
@@ -137,8 +139,9 @@ export const SingleNoteView = ({cn}) => {
           <ToolBar />
             <div className={`${className}__inner`}>
               <input type="text" className={`${className}__title`} value={singleData.title} placeholder="Title..." onChange={handleChange} />
-              {console.log(JSON.stringify(convertToRaw(editorState.getCurrentContent())))}
-              <Editor editorState={editorState} onChange={changeText} plugins={[createMarkdownPlugin()]} />
+              {console.log(JSON.stringify(editorState.getCurrentContent()))}
+
+              <Editor editorState={editorState} onChange={changeText} plugins={[createMarkdownShortcutsPlugin()]} />
             </div>
           </div>
         }
